4a9f5b8199c5698b18ce3ede8a56ccbf7b864bab
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-04-19 15:43:52 +0300

    get DEBUG from dev.env, database is now postgresql

--- skillforge/settings.py
+++ skillforge/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getattr(os.environ, ("SECRET_KEY"), None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "skillforge.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
