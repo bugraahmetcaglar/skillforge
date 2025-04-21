0e6703c1b8497fc99a71511dd11f4d7d5fc8a03f
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-04-22 00:11:59 +0300

    added ulid field with python-ulid library

--- core/fields.py
+++ core/fields.py
@@ -1,4 +1,5 @@
 from django.db import models
+import ulid
 
 
 class NullableCharField(models.CharField):
@@ -11,3 +12,25 @@ class NullableCharField(models.CharField):
         name, path, args, kwargs = super().deconstruct()
         del kwargs["null"]
         return name, path, args, kwargs
+
+
+class ULIDField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 26
+        kwargs["unique"] = kwargs.get("unique", True)
+        super().__init__(*args, **kwargs)
+    
+    def pre_save(self, model_instance, add):
+        value = getattr(model_instance, self.attname, None)
+        if value is None or value == "":
+            value = ulid.ULID()
+            setattr(model_instance, self.attname, value)
+        return value
+    
+    def to_python(self, value):
+        if isinstance(value, str) or value is None:
+            return value
+        return str(value)
+    
+    def from_db_value(self, value, expression, connection):
+        return self.to_python(value)
