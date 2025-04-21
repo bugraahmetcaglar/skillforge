e9c09aada80b201a548f95df6b9ba9f91b6f28bc
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-04-22 00:12:56 +0300

    fixed user view imports

--- apps/user/v1/views.py
+++ apps/user/v1/views.py
@@ -1,39 +1,51 @@
+from __future__ import annotations
+
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework import status, permissions
-from rest_framework.generics import (
-    CreateAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-)
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 from apps.user.models import User
-from apps.user.serializers import UserSerializer, TokenSerializer
+from apps.user.serializers import UserLoginSerializer, UserSerializer, TokenSerializer
+from core import views
 
 
-class UserRegisterAPIView(CreateAPIView):
-    queryset = User.objects.all()
+class UserRegisterAPIView(views.BaseAPIView):
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: Request, *args, **kwargs) -> Response:
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        user: User = serializer.save()
 
-        return Response({"success": True}, status=status.HTTP_201_CREATED)
+        return self.success_response(
+            data=user.token(),
+            message="User successfully registered",
+            status_code=status.HTTP_201_CREATED,
+        )
 
 
-class UserListAPIView(ListAPIView):
+class UserListAPIView(views.BaseListAPIView):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
 
 
-class UserDetailAPIView(RetrieveAPIView):
+class UserDetailAPIView(views.BaseRetrieveAPIView):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
+
+
+class UserLoginAPIView(views.BaseCreateAPIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request: Request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
 
 
 class RegenerateTokenAPIView(APIView):
