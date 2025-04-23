b9f0b8536fe4d5c33801ca7874015b6bdaf9fead
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-04-23 23:48:43 +0300

    added user login api

--- apps/user/v1/views.py
+++ apps/user/v1/views.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 from django.contrib.auth import authenticate
-from rest_framework.views import APIView
+from django.db.models import Q
 from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.request import Request
 
@@ -44,8 +46,23 @@ class UserLoginAPIView(views.BaseCreateAPIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request: Request, *args, **kwargs):
-        username = request.data.get("username", "")
-        password = request.data.get("password", "")
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        breakpoint()
+        username = serializer.validated_data["username"]
+        email = serializer.validated_data["email"]
+        password = serializer.validated_data["password"]
+
+        user = User.objects.filter(Q(username=username) | Q(email=email)).first()
+        if getattr(user, "is_active", False) is False:
+            raise AuthenticationFailed("Invalid credentials.")
+
+        user_auth = authenticate(username=user.username, password=password)
+        if not user_auth:
+            raise AuthenticationFailed("Invalid credentials.")
+
+        return self.success_response(data=user.token())
 
 
 class RegenerateTokenAPIView(APIView):
