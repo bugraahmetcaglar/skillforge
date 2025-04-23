1ecc0c0a27f3e8d2db1c3bf54101cf50a241ce38
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-04-24 01:16:00 +0300

    added staticfile and media settings

--- skillforge/urls.py
+++ skillforge/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -54,6 +55,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     try:
         import debug_toolbar
 
