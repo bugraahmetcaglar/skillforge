72682c006965b7f7399b38992f756085f94fbc5c
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-04-27 00:05:16 +0300

    datetime is changed to timezone now

--- core/models.py
+++ core/models.py
@@ -1,20 +1,17 @@
-import uuid
-import datetime
-
 from django.db import models
+from django.utils import timezone
 
 
 class SkillForgeBaseQuerySet(models.QuerySet):
     def update(self, *args, **kwargs):
         if "last_updated" not in kwargs:
-            kwargs["last_updated"] = datetime.datetime.now()
+            kwargs["last_updated"] = timezone.now()
         return super().update(**kwargs)
 
 
 class BaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(db_index=True, default=datetime.datetime.now())
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
     last_updated = models.DateTimeField(auto_now=True)
 
     class Meta:
