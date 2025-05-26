2d1660c7b5be097f18e545eae76d2263819d607a
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-05-26 21:54:09 +0300

    is_active removed from admin panel for contact

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.http.request import HttpRequest
 from apps.contact.models import Contact, ContactBackup
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "middle_name", "last_name", "email", "owner", "is_active")
+    list_display = ("first_name", "middle_name", "last_name", "email", "owner")
     list_filter = ("is_active",)
     search_fields = ("first_name", "middle_name", "last_name", "email")
     readonly_fields = ("id", "created_at", "last_updated")
@@ -18,11 +19,15 @@ class ContactAdmin(admin.ModelAdmin):
                     "middle_name",
                     "last_name",
                     "email",
-                    "phone",
-                    "address",
+                    "mobile_phone",
+                    "addresses",
                     "notes",
                 )
             },
         ),
         ("Metadata", {"fields": ("created_at", "last_updated")}),
     )
+    
+    def has_delete_permission(self, request, obj=None) -> bool:
+        # return super().has_delete_permission(obj) and obj.is_active
+        return False
