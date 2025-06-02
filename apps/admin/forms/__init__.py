73b09cc7dd1bdd48758a3026f011c5c27673bcf2
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2025-06-03 00:07:03 +0300

    remove admin

--- apps/admin/forms/__init__.py
+++
@@ -1,3 +0,0 @@
-from apps.admin.forms.user import *
-from apps.admin.forms.contact import *
-from apps.admin.forms.dashboard import *
--- apps/admin/urls/user.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from django.urls import path
-
-
-app_name = "user"
-
-urlpatterns = [
-]
